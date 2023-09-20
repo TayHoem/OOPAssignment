8faaab3382d6da90fe781c5e5baf09927b7a365b
Wyee12 <111414560+Wyee12@users.noreply.github.com>
2023-09-20 21:51:39 +0800

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author ANG
+ */
+public class Passenger {
+    private String passengerID;
+    private String passportNo;
+
+    public Passenger(String passengerID, String passportNo) {
+        this.passengerID = passengerID;
+        this.passportNo = passportNo;
+    }
+    
+    
+}
